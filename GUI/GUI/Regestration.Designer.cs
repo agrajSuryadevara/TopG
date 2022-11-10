08a0291c9260de4e47f6e983f597bb1d131c0abd
heydonttouchmyfood <agraj.suryadevara@gmail.com>
2022-11-10 15:50:19 +0000

    all pages

---
+++ GUI/GUI/Regestration.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace GUI
+{
+    partial class Regestration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Regestration));
+            this.LeftPanel = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.RegistrationPicture = new System.Windows.Forms.PictureBox();
+            this.RegistrationButton = new System.Windows.Forms.Button();
+            this.IdeasButton = new System.Windows.Forms.Button();
+            this.ClientListButton = new System.Windows.Forms.Button();
+            this.AccountButton = new System.Windows.Forms.Button();
+            this.TopPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.SearchPanel = new System.Windows.Forms.Panel();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.SearchBox = new System.Windows.Forms.TextBox();
+            this.LeftPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RegistrationPicture)).BeginInit();
+            this.TopPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SearchPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LeftPanel
+            // 
+            this.LeftPanel.BackColor = System.Drawing.Color.SlateGray;
+            this.LeftPanel.Controls.Add(this.pictureBox4);
+            this.LeftPanel.Controls.Add(this.pictureBox3);
+            this.LeftPanel.Controls.Add(this.pictureBox2);
+            this.LeftPanel.Controls.Add(this.RegistrationPicture);
+            this.LeftPanel.Controls.Add(this.RegistrationButton);
+            this.LeftPanel.Controls.Add(this.IdeasButton);
+            this.LeftPanel.Controls.Add(this.ClientListButton);
+            this.LeftPanel.Controls.Add(this.AccountButton);
+            this.LeftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftPanel.Location = new System.Drawing.Point(0, 65);
+            this.LeftPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.LeftPanel.Name = "LeftPanel";
+            this.LeftPanel.Size = new System.Drawing.Size(157, 513);
+            this.LeftPanel.TabIndex = 3;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox4.Location = new System.Drawing.Point(15, 135);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(33, 36);
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(15, 73);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(33, 36);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(15, 12);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(33, 36);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // RegistrationPicture
+            // 
+            this.RegistrationPicture.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("RegistrationPicture.BackgroundImage")));
+            this.RegistrationPicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.RegistrationPicture.Location = new System.Drawing.Point(15, 195);
+            this.RegistrationPicture.Margin = new System.Windows.Forms.Padding(2);
+            this.RegistrationPicture.Name = "RegistrationPicture";
+            this.RegistrationPicture.Size = new System.Drawing.Size(33, 36);
+            this.RegistrationPicture.TabIndex = 2;
+            this.RegistrationPicture.TabStop = false;
+            // 
+            // RegistrationButton
+            // 
+            this.RegistrationButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.RegistrationButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.RegistrationButton.FlatAppearance.BorderSize = 0;
+            this.RegistrationButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RegistrationButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegistrationButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.RegistrationButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.RegistrationButton.Location = new System.Drawing.Point(0, 183);
+            this.RegistrationButton.Margin = new System.Windows.Forms.Padding(2);
+            this.RegistrationButton.Name = "RegistrationButton";
+            this.RegistrationButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.RegistrationButton.Size = new System.Drawing.Size(157, 61);
+            this.RegistrationButton.TabIndex = 2;
+            this.RegistrationButton.Text = "   Registration";
+            this.RegistrationButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.RegistrationButton.UseVisualStyleBackColor = true;
+            this.RegistrationButton.Click += new System.EventHandler(this.RegistrationButton_Click);
+            // 
+            // IdeasButton
+            // 
+            this.IdeasButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.IdeasButton.FlatAppearance.BorderSize = 0;
+            this.IdeasButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.IdeasButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IdeasButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.IdeasButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.IdeasButton.Location = new System.Drawing.Point(0, 122);
+            this.IdeasButton.Margin = new System.Windows.Forms.Padding(2);
+            this.IdeasButton.Name = "IdeasButton";
+            this.IdeasButton.Size = new System.Drawing.Size(157, 61);
+            this.IdeasButton.TabIndex = 2;
+            this.IdeasButton.Text = "Ideas";
+            this.IdeasButton.UseVisualStyleBackColor = true;
+            // 
+            // ClientListButton
+            // 
+            this.ClientListButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ClientListButton.FlatAppearance.BorderSize = 0;
+            this.ClientListButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ClientListButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClientListButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.ClientListButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ClientListButton.Location = new System.Drawing.Point(0, 61);
+            this.ClientListButton.Margin = new System.Windows.Forms.Padding(2);
+            this.ClientListButton.Name = "ClientListButton";
+            this.ClientListButton.Size = new System.Drawing.Size(157, 61);
+            this.ClientListButton.TabIndex = 2;
+            this.ClientListButton.Text = "Client List";
+            this.ClientListButton.UseVisualStyleBackColor = true;
+            // 
+            // AccountButton
+            // 
+            this.AccountButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AccountButton.FlatAppearance.BorderSize = 0;
+            this.AccountButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AccountButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AccountButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.AccountButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AccountButton.Location = new System.Drawing.Point(0, 0);
+            this.AccountButton.Margin = new System.Windows.Forms.Padding(2);
+            this.AccountButton.Name = "AccountButton";
+            this.AccountButton.Size = new System.Drawing.Size(157, 61);
+            this.AccountButton.TabIndex = 2;
+            this.AccountButton.Text = "Account";
+            this.AccountButton.UseVisualStyleBackColor = true;
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.SlateGray;
+            this.TopPanel.Controls.Add(this.pictureBox1);
+            this.TopPanel.Controls.Add(this.SearchPanel);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(948, 65);
+            this.TopPanel.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(74, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SearchPanel
+            // 
+            this.SearchPanel.AutoScroll = true;
+            this.SearchPanel.Controls.Add(this.SearchButton);
+            this.SearchPanel.Controls.Add(this.SearchBox);
+            this.SearchPanel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.SearchPanel.Location = new System.Drawing.Point(80, 2);
+            this.SearchPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.SearchPanel.Name = "SearchPanel";
+            this.SearchPanel.Size = new System.Drawing.Size(858, 64);
+            this.SearchPanel.TabIndex = 3;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.SearchButton.FlatAppearance.BorderSize = 0;
+            this.SearchButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SearchButton.Location = new System.Drawing.Point(260, 28);
+            this.SearchButton.Margin = new System.Windows.Forms.Padding(2);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(33, 36);
+            this.SearchButton.TabIndex = 2;
+            this.SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.SearchBox.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.SearchBox.Location = new System.Drawing.Point(335, 23);
+            this.SearchBox.Margin = new System.Windows.Forms.Padding(2);
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.Size = new System.Drawing.Size(188, 20);
+            this.SearchBox.TabIndex = 3;
+            this.SearchBox.Text = "Search";
+            this.SearchBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Regestration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 578);
+            this.Controls.Add(this.LeftPanel);
+            this.Controls.Add(this.TopPanel);
+            this.Name = "Regestration";
+            this.Text = "Regestration";
+            this.LeftPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RegistrationPicture)).EndInit();
+            this.TopPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.SearchPanel.ResumeLayout(false);
+            this.SearchPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel LeftPanel;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox RegistrationPicture;
+        private System.Windows.Forms.Button RegistrationButton;
+        private System.Windows.Forms.Button IdeasButton;
+        private System.Windows.Forms.Button ClientListButton;
+        private System.Windows.Forms.Button AccountButton;
+        private System.Windows.Forms.FlowLayoutPanel TopPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel SearchPanel;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.TextBox SearchBox;
+    }
+}
